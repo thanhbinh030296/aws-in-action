0741f7201e7b59a69680d75ca47e69072e3004ac
b0ttle-mac <thanhbinh030296@gmail.com>
2024-05-13 20:34:16 +0700

    update full Chap 3

--- Chap3/tf-code/main.tf
+++ Chap3/tf-code/main.tf
@@ -15,6 +15,88 @@ resource "aws_subnet" "example" {
   cidr_block        = "10.0.0.0/20"
 }
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.selected.id
+
+  tags = {
+    Name = "public_gateway"
+  }
+}
+
+resource "aws_route_table" "r" {
+  vpc_id = aws_vpc.selected.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  route {
+    cidr_block = aws_vpc.selected.cidr_block
+    gateway_id = "local"
+  }
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_routing" {
+  subnet_id      = aws_subnet.example.id
+  route_table_id = aws_route_table.r.id
+}
+
+
+data "aws_iam_policy" "ec2ssm" {
+  arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+data "aws_iam_policy" "ssm_default_policy" {
+  arn = "arn:aws:iam::aws:policy/AmazonSSMManagedEC2InstanceDefaultPolicy"
+}
+
+resource "aws_iam_role" "ec2_ssm_role" {
+  name = "ec2-ssm-core"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "ec2-ssm-core"
+  }
+}
+
+
+resource "aws_iam_policy_attachment" "ec2_ssm_core" {
+  name = "ec-ssm-core"
+  #users      = [aws_iam_user.user.name]
+  roles = [aws_iam_role.ec2_ssm_role.name]
+  #groups     = [aws_iam_group.group.name]
+  policy_arn = data.aws_iam_policy.ec2ssm.arn
+
+}
+
+resource "aws_iam_policy_attachment" "ec2_ssm_default" {
+  name = "ec-ssm-core"
+  #users      = [aws_iam_user.user.name]
+  roles = [aws_iam_role.ec2_ssm_role.name]
+  #groups     = [aws_iam_group.group.name]
+  policy_arn = data.aws_iam_policy.ssm_default_policy.arn
+
+}
+
 
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
@@ -27,7 +109,7 @@ resource "aws_security_group" "allow_tls" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = [aws_vpc.selected.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -42,16 +124,22 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 
+resource "aws_iam_instance_profile" "ec2_ssm_iam_intsance_profile" {
+  name = "ec2_ssm_iam_intsance_profile"
+  role = aws_iam_role.ec2_ssm_role.name
+}
+
+
 resource "aws_instance" "web" {
   ami           = "ami-05b46bc4327cf9d99"
   instance_type = "t3.micro"
-  subnet_id = aws_subnet.example.id
+  subnet_id     = aws_subnet.example.id
 
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  
 
-  associate_public_ip_address =true
+  iam_instance_profile = aws_iam_instance_profile.ec2_ssm_iam_intsance_profile.name
 
+  associate_public_ip_address = true
   tags = {
     Name = "HelloWorld"
   }
